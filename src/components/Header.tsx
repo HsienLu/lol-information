1d8c1a2fd15c553d9d79b39e21fe5b0a47f07dd1
HsienLu <hsienlu@mail.ntust.edu.tw>
2024-07-11 17:32:17 +0800

    heroDetails 毛玻璃樣式

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,36 @@
-import { Link } from "react-router-dom"
+import {Link} from "react-router-dom";
 
-function Header(){
-    return(
-        <>
-            <header className="bg-white shadow-md py-4">
-      <div className="container mx-auto px-4 flex justify-between items-center">
-        <h1 className="text-2xl font-bold">英雄聯盟資訊站</h1>
-        <nav>
-          <ul className="flex space-x-4">
-            <li><Link to="/" className="text-gray-700 hover:text-gray-900">首頁</Link></li>
-            <li><Link to="/hero-detail" className="text-gray-700 hover:text-gray-900">英雄列表</Link></li>
-            <li><a href="#" className="text-gray-700 hover:text-gray-900">道具列表</a></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-        </>
-    )
+function Header() {
+  return (
+    <>
+      <header className="sticky top-0 z-50 bg-slate-700 bg-opacity-50  shadow-md py-4">
+        <div className="container mx-auto px-4 flex justify-between items-center">
+          <h1 className="text-gray-200 text-2xl font-bold">英雄聯盟資訊站</h1>
+          <nav>
+            <ul className="flex space-x-4">
+              <li>
+                <Link to="/" className="text-gray-100 hover:text-gray-300">
+                  首頁
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/hero-detail"
+                  className="text-gray-100 hover:text-gray-300"
+                >
+                  英雄列表
+                </Link>
+              </li>
+              <li>
+                <a href="#" className="text-gray-100 hover:text-gray-300">
+                  道具列表
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
 }
-export default Header
+export default Header;
--- src/pages/HeroDetails.tsx
+++ src/pages/HeroDetails.tsx
@@ -1,15 +1,12 @@
-function HeroDetails(){
-    return(
-        <>
-        <div className="bg-cover bg-center bg-no-repeat w-100 max-h-[100vh]" style={{backgroundImage: `url(${import.meta.env.VITE_IMG_URL}/img/champion/centered/Pyke_0.jpg)`}}>
+function HeroDetails() {
+    return (
+        <div className="fixed inset-0 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${import.meta.env.VITE_IMG_URL}/img/champion/centered/Pyke_0.jpg)`, backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
             <div className="container mx-auto">
-                <section>
-                <h1>123</h1>
-                    
+                <section className="g-glossy">
+                    <h1>123</h1>
                 </section>
             </div>
         </div>
-        </>
     )
 }
 export default HeroDetails

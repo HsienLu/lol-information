c8f36a99d05a144890f21e034f0d1fe57b4a1589
HsienLu <sjps946403@gmail.com>
2024-07-15 22:32:28 +0800

    調整好輪播樣式

---
+++ src/components/HeroDetailsCarousel.tsx
@@ -0,0 +1,36 @@
+import {Card, CardContent} from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export function HeroDetailsCarousel() {
+  return (
+    <Carousel className="w-full max-w-xs">
+      <CarouselContent className="">
+        {Array.from({length: 5}).map((_, index) => (
+          <CarouselItem className="" key={index}>
+            <div className="">
+              <Card className="border-0 bg-transparent">
+                <CardContent className=" flex aspect-square items-center justify-center p-1">
+                  <img
+                    className=""
+                    src={`${
+                      import.meta.env.VITE_IMG_URL
+                    }/img/champion/loading/Aatrox_0.jpg`}
+                    alt=""
+                  />
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className="bg-transparent text-slate-100 border-0 -left-8" />
+      <CarouselNext className="bg-transparent text-slate-100 border-0 -right-8" />
+    </Carousel>
+  );
+}
--- src/pages/HeroDetails.tsx
+++ src/pages/HeroDetails.tsx
@@ -1,12 +1,28 @@
+import {HeroDetailsCarousel} from "@/components/HeroDetailsCarousel";
+
 function HeroDetails() {
-    return (
-        <div className="fixed inset-0 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${import.meta.env.VITE_IMG_URL}/img/champion/centered/Pyke_0.jpg)`, backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-            <div className="container mx-auto">
-                <section className="g-glossy">
-                    <h1>123</h1>
-                </section>
-            </div>
-        </div>
-    )
+  return (
+    <div
+      className="fixed inset-0 bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage: `url(${
+          import.meta.env.VITE_IMG_URL
+        }/img/champion/centered/Pyke_0.jpg)`,
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+      }}
+    >
+      <div className="container mx-auto">
+        <section className="g-glossy">
+          <h2 className="text-center leading-normal text-3xl font-bold text-white py-5">
+            派克
+          </h2>
+          <aside className="ml-20 ">
+            <HeroDetailsCarousel></HeroDetailsCarousel>
+          </aside>
+          <main></main>
+        </section>
+      </div>
+    </div>
+  );
 }
-export default HeroDetails
+export default HeroDetails;
